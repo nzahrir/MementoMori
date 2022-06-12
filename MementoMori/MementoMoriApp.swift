708ee29f92672441bbddfec21179ecd7b148b0ba
nzahrir <nrzahrir@gmail.com>
2022-06-12 16:32:15 -0500

    Initial Commit

---
+++ MementoMori/MementoMoriApp.swift
@@ -0,0 +1,17 @@
+//
+//  MementoMoriApp.swift
+//  MementoMori
+//
+//  Created by Nassim Zahrir on 6/12/22.
+//
+
+import SwiftUI
+
+@main
+struct MementoMoriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
